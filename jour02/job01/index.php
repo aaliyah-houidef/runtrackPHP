9e761df4042f80e754c7992c66440c18becfa139
aaliyah-houidef <aaliyah.houidef@laplateforme.io>
2024-06-04 10:30:55 +0200

    developpez un algorithme qui affiche le nombre d'arguments

---
+++ jour02/job01/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+</head>
+<body>
+    <form action="index.php" method="get">
+        Jour : <input type="text" name="jour"><br>
+        Mois : <input type="text" name="mois"><br>
+        Année : <input type="text" name="année"><br>
+        <input type="submit" value="Envoyer">
+    </form>
+
+    <?php
+        if (isset($_GET)) {
+            $nb_params = count($_GET);
+            echo "Nombre d'arguments dans l'URL : $nb_params";
+        }
+    ?>
+</body>
+</html>
