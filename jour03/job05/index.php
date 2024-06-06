a41d8a2c82c7f72f758606f97c90c8145662edac
aaliyah-houidef <aaliyah.houidef@laplateforme.io>
2024-06-06 09:10:10 +0200

    push du job05

---
+++ jour03/job05/index.php
@@ -0,0 +1,21 @@
+<?php
+
+function occurrences($str, $char) {
+    $count = 0;
+    $length = strlen($str);
+
+    for ($i = 0; $i < $length; $i++) {
+        if ($str[$i] == $char) {
+            $count++;
+        }
+    }
+
+    return $count;
+}
+
+$str = "Bonjour";
+$char = "o";
+$resultat = occurrences($str, $char);
+echo "Le nombre d'occurrences de '$char' dans '$str' est : " . $resultat;
+
+?>
